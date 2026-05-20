91e3a5739e7b32efdbf4bf52206ea69fc14a1449
730852-alt <730852@student.dusd.net>
2026-05-20 13:51:40 -0700

    5.4 step 6

--- P6AguilarUnit5/Assets/Scripts/GameManager.cs
+++ P6AguilarUnit5/Assets/Scripts/GameManager.cs
@@ -12,16 +12,13 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
+    public GameObject titleScreen;
     public bool isGameActive;
     public int score;
     private float spawnRate = 1.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
 
     }
 
@@ -58,4 +55,15 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+    public void StartGame()
+    {
+        isGameActive = true;
+        score = 0;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+
+        titleScreen.gameObject.SetActive(false);
+    }
+
 }
